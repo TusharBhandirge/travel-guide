61aadd2e76104570cda3e6f367038b8b75b76741
TusharBhandirge <tusharbhandirge@gmail.com>
2021-08-17 10:11:47 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React,{useState} from "react";
 
 function App() {
+
+  
+  
+  const [emojiMeaning, setEmojiMeaning]  = useState(""); 
+
+ 
+
+  var list ={
+    "Street Food" : ["Vada Paav", "Paav Bhaji", "Puran poli"],
+    "Beaches" : ["Ganapatipule Beach", "Dahanu-Bordi Beach.","Juhu Beach."],
+    "HillStations" :["Matheran","Mahabaleshwar", "Lonavala"]
+  }
+
+  const [items,setItems] = useState("Street Food");
+
+  function sectionButtonClickhandler(section){
+      console.log("clicked "+section);
+      setItems(section);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" id="container">
+     
+     <h1>Welcome to Travel Guide of Maharashtra</h1>
+
+    <div> 
+          {
+                Object.keys(list).map( (section)=>(
+                    <button id="id-section-button"
+                      onClick={()=> sectionButtonClickhandler(section)}
+                    >
+                      {section}
+                    </button>
+                )
+                )
+          }
+      
+    </div>
+    <h2>Top three {items} in Maharashtra are : </h2>
+
+    <div id="id-div-output">
+          <ul id="id-ul-list">
+              {
+                list[items].map((item)=>(
+                  <li id="id-list-item">
+                      {item}
+                  </li>
+                )
+                
+                )
+
+              }
+          </ul>
+    </div>
+
     </div>
   );
 }
